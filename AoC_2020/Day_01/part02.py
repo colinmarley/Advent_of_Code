61ed5a52592551129a9cd3ed04d257c912efb1b1
colinmarley <colinmarley94@gmail.com>
2020-12-01 14:10:01 -0800

    Solve Day 1 Part 2

---
+++ AoC_2020/Day_01/part02.py
@@ -0,0 +1,39 @@
+# Global Array to hold the numbers as ints after being read in as str
+ints = []
+solution_exists = False
+
+#Global Constants
+DESIRED_SUM = 2020
+INPUT_FILE = "input_pt1.txt"
+
+# Read in the input file line-by line
+with open(INPUT_FILE, 'r') as file:
+    lines = file.readlines()
+
+# store the value as an integer in ints[]
+for line in lines:
+    s = int(line)
+    ints.append(s)
+
+# Have two loops to cycle through theinteger array
+#   - First cycles from beginning to end, the second cycles
+#       from the current index to the end
+#   - Adds two current values and compares to desired solution
+#       - If the sum == desired solution then print relevant information
+for i in range(0, len(ints)):
+    for j in range(i+1, len(ints)):
+        for k in range(j+1, len(ints)):
+            a = ints[i]
+            b = ints[j]
+            c = ints[k]
+            if a + b + c == DESIRED_SUM:
+                solution_exists = True
+                print("First Number (A): " + str(a))
+                print("Second Number (B): " + str(b))
+                print("Second Number (C): " + str(c))
+                print("A-Index: " + str(i))
+                print("B-Index: " + str(j))
+                print("C-Index: " + str(k))
+                print("A * B * C: " + str(a * b * c))
+
+if not solution_exists: print("No Solution")
